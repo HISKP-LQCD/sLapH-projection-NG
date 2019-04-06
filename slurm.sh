454b1ef776cd32a20b53c0ea033a6fff5d69e754
Martin Ueding <dev@martin-ueding.de>
2019-04-06 17:37:33 +0200

    Adapt driver script for ITKP workstations

--- slurm.sh
+++ slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name Proj1
+#SBATCH --job-name 001_A1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8GB
 #SBATCH --mail-user=ueding@hiskp.uni-bonn.de
@@ -10,8 +10,10 @@ set -e
 set -u
 set -x
 
+wolframscript=/usr/remote/Wolfram/Mathematica/11.3/SystemFiles/Kernel/Binaries/Linux-x86-64/wolframscript
+
 date -I seconds
 
-math -script Projection_for_rho.wls
+$wolframscript -script driver.wls "{0, 0, 1}" "A1"
 
 date -I seconds
