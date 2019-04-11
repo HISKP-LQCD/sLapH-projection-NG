0497566fb740ee71ab4f380e07ae190af0556b41
Martin Ueding <dev@martin-ueding.de>
2019-04-11 14:35:22 +0200

    Add job script generator

---
+++ template-slurm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name {{ momentum|join('') }}-{{ irrep }}
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8GB
+#SBATCH --mail-user=ueding@hiskp.uni-bonn.de
+#SBATCH --mail-type=ALL
+
+set -e
+set -u
+set -x
+
+wolframscript=/usr/remote/Wolfram/Mathematica/11.3/SystemFiles/Kernel/Binaries/Linux-x86-64/wolframscript
+if ! [[ -f "$wolframscript" ]]; then
+    wolframscript=/usr/local/Wolfram/Mathematica/11.3/SystemFiles/Kernel/Binaries/Linux-x86-64/wolframscript
+fi
+
+date -Iseconds
+
+$wolframscript -script driver.wl {{ momentum|join(' ') }} {{ irrep }}
+
+date -Iseconds
