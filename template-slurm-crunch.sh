a5560884e73a7bbda84b00379c9f974bc62cb099
Martin Ueding <dev@martin-ueding.de>
2019-04-15 12:33:05 +0200

    Create jobs for number crunching

---
+++ template-slurm-crunch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name {{ momentum|join('') }}-{{ irrep }}
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=500MB
+#SBATCH --mail-user=ueding@hiskp.uni-bonn.de
+#SBATCH --mail-type=FAIL
+
+set -e
+set -u
+set -x
+
+date -Iseconds
+
+./number_crunching.R {{ momentum|join(' ') }} {{ irrep }}
+
+date -Iseconds
