5d1628c7c63f3a821736d15b8bc70d54962a2f58
Martin Ueding <dev@martin-ueding.de>
2019-08-19 19:27:49 +0200

    Only create directory if needed

--- numeric_projection/driver.R
+++ numeric_projection/driver.R
@@ -131,6 +131,9 @@ resolve <- function (prescription) {
 
 resolved <- lapplyn(filtered_prescriptions, resolve, 6)
 
-dir.create('projected')
-resolved_filename <- sprintf('projected/resolved_%s_%s_%04d.js', total_momentum_str, irrep, config_number)
+path <- 'projected'
+if (!is.dir(path)) {
+  dir.create(path)
+}
+resolved_filename <- sprintf('%s/resolved_%s_%s_%04d.js', path, total_momentum_str, irrep, config_number)
 jsonlite::write_json(resolved, resolved_filename, pretty = TRUE)
