7b084568d6de32e879ef7ff908fb586981ba6e82
Martin Ueding <dev@martin-ueding.de>
2019-08-30 14:04:19 +0200

    Restructure numeric job scripts

--- jobscript_generator/template_slurm_numeric.sh
+++ jobscript_generator/template_slurm_numeric.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name N_{{ momentum|join('') }}_{{ irrep }}_{{ '%04d'|format(config_number) }}
+#SBATCH --job-name N_{{ '%04d'|format(config_number) }}
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=500MB
 #SBATCH --mail-user=ueding@hiskp.uni-bonn.de
 #SBATCH --mail-type=FAIL
 
-#SBATCH --output=batch_output/numeric_{{ momentum|join('') }}_{{ irrep }}_{{ config_number }}_slurm_%j.txt
+#SBATCH --output=batch_output/numeric_{{ config_number }}_slurm_%j.txt
 
 set -e
 set -u
@@ -15,6 +15,16 @@ set -x
 hostname
 date -Iseconds
 
+{% for momentum_sq, grouped in grouped2 %}
+###############################################################################
+#                                    P² = {{ momentum_sq }}                                   #
+###############################################################################
+{% for irrep, values in grouped %}
+# {{ irrep }}
+{% for _, momentum in values -%}
 /usr/bin/time {{ srcdir }}/numeric_projection/driver.R {{ momentum|join(' ') }} {{ irrep }} {{ config_number }}
+{% endfor -%}
+{% endfor -%}
+{% endfor -%}
 
 date -Iseconds
--- jobscript_generator/template_submit_numeric_jobs.sh
+++ jobscript_generator/template_submit_numeric_jobs.sh
@@ -3,16 +3,6 @@
 set -e
 set -u
 
-{% for momentum_sq, grouped in grouped2 %}
-###############################################################################
-#                                    P² = {{ momentum_sq }}                                   #
-###############################################################################
-{% for irrep, values in grouped %}
-# {{ irrep }}
-{% for _, momentum in values -%}
 {% for config_number in config_numbers -%}
-sbatch jobscripts/slurm_numeric_{{ momentum }}_{{ irrep }}_{{ '%04d'|format(config_number) }}.sh
-{% endfor -%}
-{% endfor -%}
-{% endfor -%}
+sbatch jobscripts/slurm_numeric_{{ '%04d'|format(config_number) }}.sh
 {% endfor -%}
