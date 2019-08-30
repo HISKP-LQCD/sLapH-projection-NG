10fd83fc89cfec5fc298c8d38382f5aa846b181c
Martin Ueding <dev@martin-ueding.de>
2019-08-30 16:25:51 +0200

    Fiddle with JSON merge

--- numeric_projection/numeric_utils.R
+++ numeric_projection/numeric_utils.R
@@ -19,3 +19,17 @@ get_workdir <- function () {
   
   return (NA)
 }
+
+drop_empty <- function (l) {
+  lengths <- sapply(l, length)
+  l[lengths > 0]
+}
+
+lapplyn <- function (container, f, n, ...) {
+  if (n == 1) {
+    result <- lapply(container, f, ...)
+  } else {
+    result <- lapply(container, function (x) lapplyn(x, f, n - 1, ...))
+  }
+  return (result)
+}
