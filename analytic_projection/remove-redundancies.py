d40333b78700f3771059f4959114042d7c4a3ceb
Martin Ueding <dev@martin-ueding.de>
2019-11-01 15:54:49 +0100

    Also support the C6cCD

--- analytic_projection/remove-redundancies.py
+++ analytic_projection/remove-redundancies.py
@@ -5,6 +5,7 @@
 
 import argparse
 import json
+import random
 import re
 import sys
 
@@ -12,6 +13,7 @@ import sys
 traces = {
     'C6cC': [[0, 1, 2, 3, 4, 5]],
     'C4cC': [[0, 1, 2, 3]],
+    'C6cCD': [[0, 1, 2, 3], [4, 5]],
 }
 
 
@@ -19,17 +21,38 @@ def get_ordering(sequence):
     return [index for element, index in sorted(zip(sequence, range(len(sequence))))]
 
 
-def process_name(name):
-    parts = name.split('_')
-    operators = parts[2:]
-
+def rotate_trace(operators):
     ordering = get_ordering(operators[::2])
     rotate = 2 * ordering[0]
-
     rotated = operators[rotate:] + operators[:rotate]
-    new_name = '_'.join(parts[:2] + rotated)
+    return rotated
+
+
+def process_name(name, spec):
+    parts = name.split('_')
+    operators = parts[2:]
+    new_operators = operators[:]
+
+    for trace in spec:
+        ops = [operators[i] for i in trace]
+        new_ops = rotate_trace(ops)
+
+        for i, op in zip(trace, new_ops):
+            new_operators[i] = op
+
+    new_name = '_'.join(parts[:2] + new_operators)
 
     return new_name
+
+
+def print_difference_table(names, new_names):
+    differences = [(old, new) for old, new in zip(names, new_names) if old != new]
+
+    print()
+    random.seed(0)
+    for old, new in random.sample(differences, 30):
+        print('| `{}` | `{}` |'.format(old, new))
+    print()
 
 
 def main():
@@ -38,14 +61,13 @@ def main():
     with open(options.correlator_names) as f:
         all_names = json.load(f)
 
-    for trace in traces:
+    for trace, spec in traces.items():
         names = all_names[trace]
-        new_names = [process_name(name) for name in names]
+        new_names = [process_name(name, spec) for name in names]
 
-        print(len(names))
-        print(len(set(new_names)))
+        print_difference_table(names, new_names)
 
-
+        print('| {:6} | {:6} | {:6} |'.format(trace, len(names), len(set(new_names))))
 
 
 def _parse_args():
