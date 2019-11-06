906d16a1224d600a0f66c476ccd5e86dd3831473
Martin Ueding <dev@martin-ueding.de>
2019-11-06 15:44:56 +0100

    Also remove redundancies of D diagrams

--- analytic_projection/remove-redundancies.py
+++ analytic_projection/remove-redundancies.py
@@ -11,6 +11,8 @@ import sys
 
 
 traces = {
+    'C6cD': [[0, 1], [2, 3], [4, 5]],
+    'C4cD': [[0, 1], [2, 3]],
     'C6cC': [[0, 1, 2, 3, 4, 5]],
     'C4cC': [[0, 1, 2, 3]],
     'C6cCD': [[0, 1, 2, 3], [4, 5]],
@@ -33,12 +35,23 @@ def process_name(name, spec):
     operators = parts[2:]
     new_operators = operators[:]
 
+    # Rotate the elements within each trace.
     for trace in spec:
         ops = [operators[i] for i in trace]
         new_ops = rotate_trace(ops)
 
         for i, op in zip(trace, new_ops):
             new_operators[i] = op
+
+    # For the direct (D) diagrams we can also exchange the traces because there
+    # are multiple of them. We first need to identify the traces of length 2.
+    tr2 = [i for i, tr in enumerate(spec) if len(tr) == 2]
+    if len(tr2) > 1:
+        chunks = [new_operators[2*i:2*(i+1)] for i in range(len(tr2))]
+        old_chunks = chunks[:]
+        chunks.sort()
+
+        new_operators = [op for chunk in chunks for op in chunk]
 
     new_name = '_'.join(parts[:2] + new_operators)
 
@@ -61,18 +74,33 @@ def main():
     with open(options.correlator_names) as f:
         all_names = json.load(f)
 
+    total_before = 0
+    total_after = 0
+
     for trace, spec in traces.items():
         names = all_names[trace]
         new_names = [process_name(name, spec) for name in names]
 
-        print_difference_table(names, new_names)
+        #print_difference_table(names, new_names)
 
         print('| {:6} | {:6} | {:6} |'.format(trace, len(names), len(set(new_names))))
+
+        total_before += len(names)
+        total_after += len(set(new_names))
+
+        all_names[trace] = list(set(new_names))
+
+    print()
+    print('Reduction: {} to {}, saving of {:.1f} %.'.format(total_before, total_after, 100 * (1 - total_after/total_before)))
+
+    with open(options.out, 'w') as f:
+        json.dump(all_names, f, indent=2)
 
 
 def _parse_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('correlator_names')
+    parser.add_argument('out')
     options = parser.parse_args()
 
     return options
