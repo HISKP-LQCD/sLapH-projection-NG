93782e63db0aff41d1c81c020a1b7b4ace9f69cf
Martin Ueding <dev@martin-ueding.de>
2019-11-29 16:40:57 +0100

    Open and close HDF5 files explicitly

--- numeric_projection/driver.R
+++ numeric_projection/driver.R
@@ -14,10 +14,12 @@ print(getwd())
 library(ggplot2)
 library(dplyr)
 library(magrittr)
+library(rhdf5)
 
 theme_set(theme_light())
 
 args <- commandArgs(trailingOnly = TRUE)
+cat('Command line arguments:\n  ')
 print(args)
 
 if (!exists('total_momentum')) {
@@ -39,31 +41,39 @@ names(files_list) <- diagrams
 total_momentum_sq <- sum(total_momentum^2)
 total_momentum_str <- paste0(sprintf('%d', total_momentum), collapse = '')
 
-
 prescription_filename <- sprintf('prescriptions/prescription_%s_%s.js', total_momentum_str, irrep)
+cat('Reading all prescriptions …\n')
 all_prescriptions <- jsonlite::read_json(prescription_filename)
+cat('  Done\n')
 
+cat('Building list of needed dataset names …\n')
 needed_names <- unique(unlist(lapplyn(all_prescriptions, function (rule) rule$datasetname, 7)))
+cat('  Done\n')
+
+cat('Opening HDF5 files …\n')
+file_handles <- list()
+for (diagram in diagrams) {
+  path <- files_list[[diagram]]
+  stopifnot(file.exists(path))
+  print(path)
+  file_handles[[diagram]] <- H5Fopen(path, flags = 'H5F_ACC_RDONLY')
+}
+cat('  Done\n')
 
 load_dataset <- function (datasetname) {
     diagram <- strsplit(datasetname, '_')[[1]][1]
-    
-    filename <- files_list[[diagram]]
-    stopifnot(file.exists(filename))
-    
-    #print(sprintf('Fetching %s (a %s) from %s.\n', datasetname, diagram, filename))
     
     skip_h5_errors <- FALSE
     
     if (skip_h5_errors) {
       dataset <- tryCatch(
-        rhdf5::h5read(filename, datasetname),
+        h5read(file_handles[[diagram]], datasetname),
         error = function (e) {
           warning(e)
           NA
         })
     } else {
-      dataset <- rhdf5::h5read(filename, datasetname)
+      dataset <- h5read(file_handles[[diagram]], datasetname)
     }
     
     if (length(dataset) == 1 && is.na(dataset)) {
@@ -79,9 +89,14 @@ load_dataset <- function (datasetname) {
     }
 }
 
+cat('Loading correlators from HDF5 files …\n')
 needed_raw <- lapply(needed_names, load_dataset)
-
 names(needed_raw) <- needed_names
+cat('  Done\n')
+
+cat('Closing HDF5 files …\n')
+invisible(lapply(file_handles, H5Fclose))
+cat('  Done\n')
 
 drop_zero_length <- function (x) {
   x[sapply(x, length) > 0]
@@ -118,7 +133,9 @@ resolve <- function (prescription) {
   apply(m, 2, sum)
 }
 
+cat('Projecting …\n')
 resolved <- lapplyn(filtered_prescriptions, resolve, 6)
+cat('  Done\n')
 
 drop_small <- function (corr_list) {
   firsts <- sapply(corr_list, function (corr) corr[1])
@@ -139,5 +156,7 @@ path <- 'projected'
 if (!dir.exists(path)) {
   dir.create(path)
 }
+cat('Writing JSON …\n')
 resolved_filename <- sprintf('%s/resolved_%s_%s_%04d.js', path, total_momentum_str, irrep, config_number)
 jsonlite::write_json(filtered, resolved_filename, pretty = TRUE, digits = NA)
+cat('  Done\n')
