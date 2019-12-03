fa2ea9b69661b5587c3903e34db6dff33d54d34e
Martin Ueding <dev@martin-ueding.de>
2019-12-03 09:22:08 +0100

    Request more memory

--- numericprojection/inst/projected_merge_slurm.sh
+++ numericprojection/inst/projected_merge_slurm.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name projected_merge
 #SBATCH --time=1-00:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=950MB
+#SBATCH --mem=50GB
 #SBATCH --mail-user=ueding@hiskp.uni-bonn.de
 #SBATCH --mail-type=FAIL
 
 #SBATCH --output=batch_output/projected_merge_slurm_%j.txt
 
-Rscript -e 'numericprojection::projected_merge()'
+/usr/bin/time Rscript -e 'numericprojection::projected_merge()'
